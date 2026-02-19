15d72f292b94ef969008e4cf118b7fdcb07b38d3
Cosik <sentyrz.m@gmail.com>
2026-02-19 18:55:57 +0100

    bump version for new feature

--- stable/hoymilesapi.py
+++ stable/hoymilesapi.py
@@ -382,13 +382,13 @@ class Hoymiles(object):
         Returns:
             bool: Status of getting token operation
         """
-        status, token = ArgonToken().get_token(
+        status, token = LegacyToken().get_token(
             self._config["HOYMILES_USER"], self._config["HOYMILES_PASSWORD"]
         )
         if status:
             return True, token
 
-        return LegacyToken().get_token(
+        return ArgonToken().get_token(
             self._config["HOYMILES_USER"], self._config["HOYMILES_PASSWORD"]
         )
 
